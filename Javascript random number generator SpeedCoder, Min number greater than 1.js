76aeba53f08e1274208428fd73f816b1273301c2
Hueston94 <30508835+Hueston94@users.noreply.github.com>
2019-08-06 00:58:56 +0100

    Add files via upload

---
+++ Javascript random number generator SpeedCoder, Min number greater than 1.js
@@ -0,0 +1,17 @@
+//function randomIntFromInterval(min, max) { // min and max included 
+//  return Math.floor(Math.random() * (max - min + 1) + min);
+//}
+
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+var int1 = randomIntFromInterval(80, 81);
+var str1 = "http://www.speedcoder.net/lessons/customcode/";
+var str2 = int1.toString();
+var str3 = "/";
+var res = str1.concat(str2, str3);
+window.location.href = res;
+  
+// Created Bookmarklet using https://mrcoles.com/bookmarklet/
+  
